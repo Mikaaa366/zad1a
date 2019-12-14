298734030728c38f054098f5fb05c60007e75ee1
Dominika <mikaaa366@gmail.com>
2019-12-14 09:51:49 +0100

    budowanie klasy

--- main.cpp
+++ main.cpp
@@ -1,27 +1,6 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <thread>
-
-
+#include "tstring.h"
+#include<iostream>
 using namespace std;
-void addstring(unsigned n, string&s){
-    while (n--)(
-        s+="*"; cout <<"A";
-    )
-}
-void removestring (string&s){
-    while (!s,empty()){
-        s.erase(s.end()-1); cout<<"B";
-
-    }
-}
-
-int main() {
-	string m;
-    thread t1(addstring, 100, ref(m));
-    thread t2(removestring, ref(m));
-    t1.join();
-    t2.join();
-    }
+int main(){
+    TString s1;
 }
